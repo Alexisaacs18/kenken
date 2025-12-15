585d406ca73ffef0b4ab4bbb4ce9c64b80620807
Alex Isaacs <alexisaacs18@gmail.com>
2025-12-15 16:10:30 -0500

    edits

--- backend/.wrangler/tmp/dev-N4nFZM/index.js
+++ backend/.wrangler/tmp/dev-N4nFZM/index.js
@@ -1,7 +1,7 @@
 var __defProp = Object.defineProperty;
 var __name = (target, value) => __defProp(target, "name", { value, configurable: true });
 
-// .wrangler/tmp/bundle-eqaZO9/strip-cf-connecting-ip-header.js
+// .wrangler/tmp/bundle-gBHEsH/strip-cf-connecting-ip-header.js
 function stripCfConnectingIPHeader(input, init) {
   const request = new Request(input, init);
   request.headers.delete("CF-Connecting-IP");
@@ -592,7 +592,7 @@ var jsonError = /* @__PURE__ */ __name(async (request, env, _ctx, middlewareCtx)
 }, "jsonError");
 var middleware_miniflare3_json_error_default = jsonError;
 
-// .wrangler/tmp/bundle-eqaZO9/middleware-insertion-facade.js
+// .wrangler/tmp/bundle-gBHEsH/middleware-insertion-facade.js
 var __INTERNAL_WRANGLER_MIDDLEWARE__ = [
   middleware_ensure_req_body_drained_default,
   middleware_miniflare3_json_error_default
@@ -624,7 +624,7 @@ function __facade_invoke__(request, env, ctx, dispatch, finalMiddleware) {
 }
 __name(__facade_invoke__, "__facade_invoke__");
 
-// .wrangler/tmp/bundle-eqaZO9/middleware-loader.entry.ts
+// .wrangler/tmp/bundle-gBHEsH/middleware-loader.entry.ts
 var __Facade_ScheduledController__ = class {
   constructor(scheduledTime, cron, noRetry) {
     this.scheduledTime = scheduledTime;
